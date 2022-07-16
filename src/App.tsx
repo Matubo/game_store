0729deba6e1f6a2bf3afb23020578968076ef1fd
Matubo <matbei14@yandex.ru>
2022-07-16 22:54:33 +0300

    moved test component to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { useState } from 'react';
 import ApiTest from './ApiTest';
 import './App.css';
 import StoreTest from './StoreTest';
+import TestBlock from './TestBlock';
 
 export default function App() {
   return (
     <div className="React-App">
-      <StoreTest></StoreTest>
-      <ApiTest></ApiTest>
+      <TestBlock></TestBlock>
     </div>
   );
 }
---
+++ src/TestBlock.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import ApiTest from './ApiTest';
+import StoreTest from './StoreTest';
+
+export default function TestBlock() {
+  const [testActive, setTestActive] = useState(false);
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0 }}>
+      <button
+        onClick={() => {
+          setTestActive(!testActive);
+        }}
+      >
+        Test
+      </button>
+      {testActive ? (
+        <div className="test_block">
+          <StoreTest></StoreTest>
+          <ApiTest></ApiTest>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
