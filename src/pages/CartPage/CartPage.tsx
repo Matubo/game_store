52149fbbacca60a2babbc38de0354a7fcf272237
Чупров Матвей Николаевич <spb-chuprov.mn@main.local>
2022-08-18 11:54:37 +0300

    add cart clear button, change meta content properties

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
 import { useAppDispatch } from 'src/hooks/useTypedDispatch';
 import { useTypedSelector } from 'src/hooks/useTypedSelector';
-import { decreaseItemQuantity } from 'src/redux/reducers/cartReducer';
+import { decreaseItemQuantity, clearCart } from 'src/redux/reducers/cartReducer';
 import err_img from '../../assets/img/error_img/no_image_avaliable.jpg';
 import './CartPage.scss';
 
 export default function CartPage() {
   const dispatch = useAppDispatch();
   const { cartItems, totalPrice, totalQuantity } = useTypedSelector((state) => state.cart);
+  /*   const {} = useTypedSelector((state) => state.user.user); */
 
-  const delClickHandler = (id: number) => {
+  const decreaseClickHandler = (id: number) => {
     dispatch(decreaseItemQuantity({ id }));
+  };
+
+  const clearCartClickHandler = () => {
+    dispatch(clearCart());
   };
 
   const imgErrorHandler = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
@@ -20,48 +24,56 @@ export default function CartPage() {
 
   if (cartItems.length > 0)
     return (
-      <table className="cart-table">
-        <thead className="cart-table__head">
-          <tr>
-            <th>img</th>
-            <th>name</th>
-            <th>count</th>
-            <th>price</th>
-            <th>-</th>
-          </tr>
-        </thead>
-        <tbody className="cart-table__body">
-          {cartItems.map((elem, i) => {
-            return (
-              <tr key={elem.id}>
-                <td className="cart-tale__img-container">
-                  <img className="img-container__img" src={elem.image} onError={imgErrorHandler}></img>
-                </td>
-                <td>{elem.name}</td>
-                <td>{elem.amount}</td>
-                <td>{elem.price}</td>
-                <td>
-                  <button
-                    className="cart-table__subt-button"
-                    onClick={() => {
-                      delClickHandler(elem.id);
-                    }}
-                  >
-                    -
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-          <tr>
-            <td className="cart-table__empty-td"></td>
-            <td className="cart-table__empty-td"></td>
-            <td>{totalQuantity}</td>
-            <td>{totalPrice}</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="cart">
+        <table className="cart__table cart-table">
+          <thead className="cart-table__head">
+            <tr>
+              <th>img</th>
+              <th>name</th>
+              <th>count</th>
+              <th>price</th>
+              <th>-</th>
+            </tr>
+          </thead>
+          <tbody className="cart-table__body">
+            {cartItems.map((elem, i) => {
+              return (
+                <tr key={elem.id}>
+                  <td className="cart-tale__img-container">
+                    <img className="img-container__img" src={elem.image} onError={imgErrorHandler}></img>
+                  </td>
+                  <td>{elem.name}</td>
+                  <td>{elem.amount}</td>
+                  <td>{elem.price}</td>
+                  <td>
+                    <button
+                      className="cart-table__subt-button"
+                      onClick={() => {
+                        decreaseClickHandler(elem.id);
+                      }}
+                    >
+                      -
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td className="cart-table__empty-td"></td>
+              <td className="cart-table__empty-td"></td>
+              <td>{totalQuantity}</td>
+              <td>{totalPrice}</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <button className="cart__clear-button" onClick={clearCartClickHandler}>
+          Очистить корзину
+        </button>
+        <button className="cart__buy-button" onClick={() => {}}>
+          Оформить
+        </button>
+      </div>
     );
 
   return <p>cart is empty</p>;
