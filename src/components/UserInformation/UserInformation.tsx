3c11101410f67f3405ecafd985449f2f49414043
Matubo <matbei14@yandex.ru>
2022-08-28 23:15:48 +0300

    add foto upload

--- src/components/UserInformation/UserInformation.tsx
+++ src/components/UserInformation/UserInformation.tsx
@@ -33,9 +33,28 @@ export default function UserInformation({ changeUserData, userData }: IProps) {
     setBioState({ ...bioState, description: e.target.value });
   };
 
+  const changeAvatarHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.currentTarget.files[0];
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      console.log(reader);
+      if (file.type.match('image.*')) {
+        if (file.size < 358400) {
+          setBioState({ ...bioState, avatar: reader.result as string });
+        } else {
+          alert('Too big');
+        }
+      } else {
+        alert('NOT a FOTO O_o');
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div className="user-page">
       <img src={bioState.avatar ? bioState.avatar : defaultImg}></img>
+      <input type="file" onChange={changeAvatarHandler} disabled={disabled} />
       <p>Your login : {username}</p>
       <p>Your name :</p>
       <input type="text" value={bioState.name} disabled={disabled} onChange={changeNameHandler}></input>
